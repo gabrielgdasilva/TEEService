bbeabf3c3e08c72e75e4f172f3e76820cacd66f3
Gabriel Silva <20029646@anhembimorumbi.edu.br>
2016-04-24 13:58:23 -0300

    Incluindo novo programa. WebService

---
+++ TEEService/DataContracts/Bandeira.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Runtime.Serialization;
+
+namespace TEEService.DataContracts
+{
+    [DataContract]
+    public class Bandeira
+    {
+        [DataMember]
+        public int BandeiraID { get; set; }
+        [DataMember]
+        public string BandeiraString { get; set; }
+    }   
+}
